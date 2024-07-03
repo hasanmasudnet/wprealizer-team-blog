51bdfb5ab74dae5a05694506e55a7a3a9e70f06a
jahidcsebd <dev.jahidcsebd@gmail.com>
2024-07-03 19:46:31 +0600

    init commit

--- inc/menu-register.php
+++
@@ -1,5 +0,0 @@
-<?php
-//Register Nav Menu
-register_nav_menus(array(
-    'primary_menu' => __('Primary Menu', 'blogpress'),
-));
--- index.php
+++
@@ -1,7 +0,0 @@
-<?php get_header(); ?>
-
-<!-- Main -->
-
-
-<!-- Footer -->
-<?php get_footer(); ?>
